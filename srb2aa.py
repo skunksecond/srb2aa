6c551570be3fb3a185b46bd38b5447fc703367c5
skunksecond <74111880+skunksecond@users.noreply.github.com>
2025-03-23 18:15:38 -0500

    Update srb2aa.py
    get rid of print because it's not neccssary

--- srb2aa.py
+++ srb2aa.py
@@ -2,8 +2,6 @@ import os
 
 alist = [a for a in os.listdir("addons") if os.path.isfile(os.path.join("addons", a))]
 
-print(alist)
-
 try:
     f = open("autoexec.cfg", "x+")
 except(FileExistsError):
